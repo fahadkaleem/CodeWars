28cd6a1104644efb0049cc74106503dc991d574d
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2017-05-28 14:09:09 -0500

    Added Solutions

---
+++ 8 kyu/Simple Fun #261: Whose Move.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/59126992f9f87fd31600009b
+
+def whoseMove(lastPlayer, win):
+    players = {"white": "black", "black": "white"}
+    if win:
+        return lastPlayer
+    return players[lastPlayer]
+
+    # Your Move...
