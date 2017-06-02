e619a637ceb5cf36ace89a5abd8127f97a35ea0b
Mohammed Fahad Kaleem <mohammedfahadkaleem@gmail.com>
2017-06-02 11:04:37 -0500

    Added Solutions

---
+++ 8 kyu/Keep up the hoop.py
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+
+def hoopCount(n):
+    if n>=10:
+        return "Great, now move on to tricks"
+    else:
+        return "Keep at it until you get it"
